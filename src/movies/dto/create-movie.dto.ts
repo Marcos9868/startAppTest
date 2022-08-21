c037f450fca10e5b65ed6f9e1b348c40946442ae
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-08-21 11:13:57 -0300

    feat: Add the decorator @ApiProperty for the schema to be visible

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,18 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsOptional, MinLength } from 'class-validator';
 
 export class CreateMovieDto {
+  @ApiProperty()
   @IsNotEmpty()
   @MinLength(4)
   readonly title: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsOptional()
   @MinLength(4)
   readonly gender: string;
 
+  @ApiProperty()
   @IsOptional()
   readonly release: number;
 
+  @ApiProperty()
   @IsOptional()
   readonly imdb: number;
 }
