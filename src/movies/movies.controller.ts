44a1178a0c0c04888b742dc7b28e27eb2327d7bb
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-08-21 20:44:53 -0300

    fix: Remove @ApiBody decorator

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -14,18 +14,17 @@ import { MoviesService } from './movies.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
-import { ApiBearerAuth, ApiBody } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 @Controller('movies')
+@ApiTags('Movies')
 export class MoviesController {
   constructor(private readonly moviesService: MoviesService) {}
 
-  @UseGuards(JwtAuthGuard)
   @ApiBearerAuth('access-token')
+  @UseGuards(JwtAuthGuard)
   @Post()
-  @ApiBody({ type: [CreateMovieDto] })
-  create(@Body() createMovieDto: CreateMovieDto, @Req() request: any) {
-    console.log(request.user);
+  create(@Body() createMovieDto: CreateMovieDto) {
     return this.moviesService.create(createMovieDto);
   }
 
@@ -41,18 +40,19 @@ export class MoviesController {
 
   @Get(':title')
   findOneBy(@Param('title') title: string) {
+    console.log(title);
     return this.moviesService.findOneByTitle(title);
   }
 
   @Get(':gender')
   findOneBY(@Param('gender') gender: string) {
+    console.log(gender);
     return this.moviesService.findOneByGender(gender);
   }
 
   @ApiBearerAuth('access-token')
   @UseGuards(JwtAuthGuard)
   @Patch(':id')
-  @ApiBody({ type: [UpdateMovieDto] })
   update(@Param('id') id: number, @Body() updateMovieDto: UpdateMovieDto) {
     return this.moviesService.update(+id, updateMovieDto);
   }
