ad235b1dbf8b24b133da532b542486fd5a91c790
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-08-21 20:47:20 -0300

    feat: Add all api modules

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmAsyncConfig } from './config/typeorm.config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MoviesModule } from './movies/movies.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      isGlobal: true,
+      cache: true,
+    }),
+    TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
+    MoviesModule,
+    AuthModule,
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
