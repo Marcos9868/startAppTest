316e24936eab45eb4d6a4093345f55858f7238c6
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-08-22 20:06:06 -0300

    fix: Fix query and structure problems

--- movies-startapp/src/components/movies/Movies.tsx
+++ movies-startapp/src/components/movies/Movies.tsx
@@ -1,70 +1,100 @@
-import { Table, Tr, Td, MovieBox, Img } from "./styles";
-import axios from "axios";
+import { Table, Tr, Td, MovieBox, Img } from './styles';
+import axios from 'axios';
 import { useState, useEffect } from 'react';
-import { IMovies } from "src/interfaces/IMovies";
+import { IMovies } from 'src/interfaces/IMovies';
 
 const Movies = () => {
-
   const movieMock = [
-  {
-    "id": 1,
-    "title": "Death Race",
-    "gender": "Race",
-    "release": 2013,
-    "imdb": 7.2
-  },
-  {
-    "id": 2,
-    "title": "Lord Of The Rings: Return Of The King",
-    "gender": "Fiction",
-    "release": 2004,
-    "imdb": 8.8
-  },
-  {
-    "id": 3,
-    "title": "Star Wars: Return of The Sith",
-    "gender": "Fiction",
-    "release": 1999,
-    "imdb": 8.6
-  },
-  {
-    "id": 4,
-    "title": "Lord Of The Rings: The Two Thrones",
-    "gender": "Fiction",
-    "release": 2002,
-    "imdb": 8.2
-  },
-  {
-    "id": 5,
-    "title": "The Expendables",
-    "gender": "Action",
-    "release": 2010,
-    "imdb": 7.6
-  }
-]
-  
-  const [movies , setMovies] = useState<IMovies[]>([]);
+    {
+      id: 1,
+      title: 'Death Race',
+      gender: 'Race',
+      release: 2013,
+      imdb: 7.2,
+    },
+    {
+      id: 2,
+      title: 'Lord Of The Rings: Return Of The King',
+      gender: 'Fiction',
+      release: 2004,
+      imdb: 8.8,
+    },
+    {
+      id: 3,
+      title: 'Star Wars: Return of The Sith',
+      gender: 'Fiction',
+      release: 1999,
+      imdb: 8.6,
+    },
+    {
+      id: 4,
+      title: 'Lord Of The Rings: The Two Thrones',
+      gender: 'Fiction',
+      release: 2002,
+      imdb: 8.2,
+    },
+    {
+      id: 5,
+      title: 'The Expendables',
+      gender: 'Action',
+      release: 2010,
+      imdb: 7.6,
+    },
+  ];
+
+  const [movies, setMovies] = useState<IMovies[]>([]);
 
   async function getAllMovies() {
-    await axios.get('http://localhost:3333/movies')
-    .then((response) => setMovies(response.data))
-    .catch((err) => console.log(err));
-  } 
+    await axios
+      .get('http://localhost:3333/movies')
+      .then((response) => setMovies(response.data))
+      .catch((err) => console.log(err));
+  }
 
-  useEffect(() => {
-  }, [])
+  useEffect(() => {}, []);
 
-  console.log(movies)
+  console.log(movies);
 
   return (
-    <div>
-      {movieMock.map((movie, index) => (
-        <div key={index}>
-          <h1>{movie.title}</h1>
-        </div>
-      ))}
-    </div>  
+    <Table>
+      <thead>
+        <tr>
+          {Object.keys(movieMock[0]).map((item) => (
+            <th>{item}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {movieMock.map((movie) => (
+          <tr key={movie.id}>
+            {Object.values(movie).map((item, index) => (
+              <td key={index}>{item}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   );
-}
+};
 
 export default Movies;
+
+// {movies?.map((movie: any) => {
+//   <div>
+//     <div>
+//       <h3>{movie.title}</h3>
+//       <Img src="" alt="No Image"/>
+//       <h4>{movie.gender}</h4>
+//       <h5>{movie.release}</h5>
+//       <h5>{movie.imdb}</h5>
+//     </div>
+//   </div>
+// })}
+
+{
+  /* <h3>{item.title}</h3>
+              <Img src="" alt="No Image"/>
+              <h4>{movie.gender}</h4>
+              <h5>{movie.release}</h5>
+              <h5>{movie.imdb}</h5> */
+}
