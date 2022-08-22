150e614a1b88f9d76227edf7edd525d8435c8992
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-08-22 15:27:10 -0300

    feat: Add movies component

--- movies-startapp/src/components/movies/Movies.tsx
+++ movies-startapp/src/components/movies/Movies.tsx
@@ -1,11 +1,64 @@
-
+import { Table, Tr, Td, MovieBox, Img } from "./styles";
 
 const Movies = () => {
   return (
     <div>
-      <ul>
-        <li>Movie #1</li>
-      </ul>
+      <Table>
+        <Tr>
+          <Td>
+            <MovieBox>
+              <h3>Movie #1</h3>
+              <Img src="" alt="No Image"/>
+              <h4>Gender</h4>
+              <h5>IMDB</h5>
+            </MovieBox>
+          </Td>
+          <Td>
+            <MovieBox>
+              <h3>Movie #2</h3>
+              <Img src="" alt="No Image"/>
+              <h4>Gender</h4>
+              <h5>IMDB</h5>
+            </MovieBox>
+          </Td>
+        </Tr>
+        <Tr>
+          <Td>
+            <MovieBox>
+              <h3>Movie #3</h3>
+              <Img src="" alt="No Image"/>
+              <h4>Gender</h4>
+              <h5>IMDB</h5>
+            </MovieBox>
+          </Td>
+          <Td>
+            <MovieBox>
+              <h3>Movie #4</h3>
+              <Img src="" alt="No Image"/>
+              <h4>Gender</h4>
+              <h5>IMDB</h5>
+            </MovieBox>
+          </Td>
+        </Tr>
+        <Tr>
+          <Td>
+            <MovieBox>
+              <h3>Movie #5</h3>
+              <Img src="" alt="No Image"/>
+              <h4>Gender</h4>
+              <h5>IMDB</h5>
+            </MovieBox>
+          </Td>
+          <Td>
+            <MovieBox>
+              <h3>Movie #6</h3>
+              <Img src="" alt="No Image"/>
+              <h4>Gender</h4>
+              <h5>IMDB</h5>
+            </MovieBox>
+          </Td>
+        </Tr>
+      </Table>
     </div>
   );
 }
