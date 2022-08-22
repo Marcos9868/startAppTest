5b380224f6f5baa80bc8c6c3b635e957a4eae21c
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-08-22 18:50:17 -0300

    feat: Add get call on component, renderize data of backend

--- movies-startapp/src/components/movies/Movies.tsx
+++ movies-startapp/src/components/movies/Movies.tsx
@@ -1,65 +1,69 @@
 import { Table, Tr, Td, MovieBox, Img } from "./styles";
+import axios from "axios";
+import { useState, useEffect } from 'react';
+import { IMovies } from "src/interfaces/IMovies";
 
 const Movies = () => {
+
+  const movieMock = [
+  {
+    "id": 1,
+    "title": "Death Race",
+    "gender": "Race",
+    "release": 2013,
+    "imdb": 7.2
+  },
+  {
+    "id": 2,
+    "title": "Lord Of The Rings: Return Of The King",
+    "gender": "Fiction",
+    "release": 2004,
+    "imdb": 8.8
+  },
+  {
+    "id": 3,
+    "title": "Star Wars: Return of The Sith",
+    "gender": "Fiction",
+    "release": 1999,
+    "imdb": 8.6
+  },
+  {
+    "id": 4,
+    "title": "Lord Of The Rings: The Two Thrones",
+    "gender": "Fiction",
+    "release": 2002,
+    "imdb": 8.2
+  },
+  {
+    "id": 5,
+    "title": "The Expendables",
+    "gender": "Action",
+    "release": 2010,
+    "imdb": 7.6
+  }
+]
+  
+  const [movies , setMovies] = useState<IMovies[]>([]);
+
+  async function getAllMovies() {
+    await axios.get('http://localhost:3333/movies')
+    .then((response) => setMovies(response.data))
+    .catch((err) => console.log(err));
+  } 
+
+  useEffect(() => {
+  }, [])
+
+  console.log(movies)
+
   return (
     <div>
-      <Table>
-        <Tr>
-          <Td>
-            <MovieBox>
-              <h3>Movie #1</h3>
-              <Img src="" alt="No Image"/>
-              <h4>Gender</h4>
-              <h5>IMDB</h5>
-            </MovieBox>
-          </Td>
-          <Td>
-            <MovieBox>
-              <h3>Movie #2</h3>
-              <Img src="" alt="No Image"/>
-              <h4>Gender</h4>
-              <h5>IMDB</h5>
-            </MovieBox>
-          </Td>
-        </Tr>
-        <Tr>
-          <Td>
-            <MovieBox>
-              <h3>Movie #3</h3>
-              <Img src="" alt="No Image"/>
-              <h4>Gender</h4>
-              <h5>IMDB</h5>
-            </MovieBox>
-          </Td>
-          <Td>
-            <MovieBox>
-              <h3>Movie #4</h3>
-              <Img src="" alt="No Image"/>
-              <h4>Gender</h4>
-              <h5>IMDB</h5>
-            </MovieBox>
-          </Td>
-        </Tr>
-        <Tr>
-          <Td>
-            <MovieBox>
-              <h3>Movie #5</h3>
-              <Img src="" alt="No Image"/>
-              <h4>Gender</h4>
-              <h5>IMDB</h5>
-            </MovieBox>
-          </Td>
-          <Td>
-            <MovieBox>
-              <h3>Movie #6</h3>
-              <Img src="" alt="No Image"/>
-              <h4>Gender</h4>
-              <h5>IMDB</h5>
-            </MovieBox>
-          </Td>
-        </Tr>
-      </Table>
-    </div>
+      {movieMock.map((movie, index) => (
+        <div key={index}>
+          <h1>{movie.title}</h1>
+        </div>
+      ))}
+    </div>  
   );
 }
 
