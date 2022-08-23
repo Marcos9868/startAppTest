cba58fba8067c17b9071a97f51a1bd8ea5b41c80
Marcos9868 <marcos_m_ferreira@outlook.com>
2022-08-23 18:31:20 -0300

    fix: Extends movie entity

--- src/movies/entities/movie.entity.ts
+++ src/movies/entities/movie.entity.ts
@@ -1,7 +1,7 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class Movie {
+export class Movie extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
